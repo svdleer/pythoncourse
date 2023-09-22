1395894042ac311ca0dbd00fb000e615253790ce
Silvester van der leer <silvester@vdleer.nl>
2023-09-22 16:28:53 +0200

    day 5 commit

--- napalm-get-interfaces-ip.py
+++ napalm-get-interfaces-ip.py
@@ -1,6 +1,7 @@
 import json
 import yaml
-
+import pprint
+yaml.version = "1.1"
 from napalm import get_network_driver
 
 user = 'sntuser'
@@ -12,8 +13,30 @@ driver = get_network_driver('ios')
 iosvl2 = driver('10.99.99.15', user, password)
 iosvl2.open()
 
+#yamltemplate =  [ {'get_interfaces_ip'.  } ]
 ios_output = iosvl2.get_interfaces_ip()
-print(yaml.dump(ios_output))
 
 
 
+
+
+compliance_dict={'get_interfaces_ip': {}}
+
+for key, val in dict.items(ios_output):
+        compliance_dict['get_interfaces_ip'][key] = val
+
+print(yaml.dump(ios_output, default_flow_style=True,explicit_start=True))
+
+#test123 = {**yamltemplate, **ios_output}
+#yamltemplate.update['get_interfaces_ip'] = ios_output
+
+
+#print (yaml.dump(yamltemplate))
+
+#compliance_dict.update['get_interfaceip'] = ios_output
+
+#print(yaml.dump(compliance,explicit_start = "True"))
+
+#print (yaml_data)
+
+#broken will be fixed after monday
